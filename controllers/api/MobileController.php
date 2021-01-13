b6b9ae8f339d9a221dad53897838eb9918dec853
Hadi <fayadhadi2014@gmail.com>
2021-01-13 23:16:00 +0200

    delegate

--- controllers/api/MobileController.php
+++ controllers/api/MobileController.php
@@ -2,6 +2,9 @@
 
 namespace app\controllers\api;
 
+use app\models\Users;
+use Yii;
+
 class MobileController extends ApiController {
 
     public function actionCreatePost() {
@@ -10,6 +13,53 @@ class MobileController extends ApiController {
 
         $text = $post["text"];
         return $text;
+    }
+
+    public function actionSignup() {
+
+        $post = Yii::$app->request->post();
+        $fullname = $post["fullname"];
+        $c_phone = $post["c_phone"];
+        $email = $post["email"];
+        $password = $post["password"];
+        $username = $post["username"];
+
+
+        //return $post;
+        $user = new Users();
+        $user->username = $username;
+        $user->fullname = $fullname;
+        $user->c_phone = $c_phone;
+        $user->password = $password;
+        $user->email = $email;
+//
+//        $user = new Users();
+//        $user->username = "hadifd";
+//        $user->fullname = "hadifd";
+//        $user->c_phone = "7171";
+//        $user->password = "pass";
+//        $user->email = "email";
+        $user->save();
+        if ($user->save()) {
+            return true;
+        } else
+            return "faileddd";
+    }
+
+    public function actionLogin() {
+
+        $post = Yii::$app->request->post();
+        $username = $post["username"];
+        $password = $post["password"];
+
+        $user = Users::find()
+                ->where(["username" => $username])
+                ->andWhere(["password" => $password])
+                ->one();
+        if ($user) {
+            return $user;
+        } else
+            return false;
     }
 
 }
